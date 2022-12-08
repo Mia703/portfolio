a506f5a09fc7b5110de877665fc70c93ec9f02c4
Mia703 <bookworm7572@gmail.com>
2022-12-07 21:57:21 -0500

    mobile tablet desktop styles

---
+++ actions/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	/* =================== changes colour span =================== */
+	$(".colour").delay(10000).animate({
+		color: '#DF7C7D',
+		textShadow: '0 0 .65px #DF7C7D',
+		transistion: 'all .5s ease-in-out'
+	});
+});

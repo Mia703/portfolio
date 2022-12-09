2cd0149e6bff2a3e238e177470612575cfa8196a
Mia703 <bookworm7572@gmail.com>
2022-12-09 10:14:16 -0500

    updated main heading

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-	/* =================== changes colour span =================== */
-	$(".colour").delay(10000).animate({
-		color: '#DF7C7D',
-		textShadow: '0 0 .65px #DF7C7D',
-		transistion: 'all .5s ease-in-out'
-	});
+	
+	
 });

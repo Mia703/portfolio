067191a5c55b4fe92d6881440a66054e4c80b1e6
Mia703 <bookworm7572@gmail.com>
2022-12-12 14:55:12 -0500

    Mobile Style Updates
    - Changed the style again
    - added mobile menu and scroll
    - need to add about me at bottom of page
    - need to add contact me section

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,46 @@
 $(document).ready(function () {
 
-	
-	
+	// open mobile menu
+	$(".menu-toggle.open").click(function (e) { 
+		e.preventDefault();
+
+		$("#overlay").show(
+			'slide', {direction: 'right'},
+			'slow'
+		);
+
+		$("#mobile.navigation").show(
+			'slide', {direction: 'right'}, 
+			'slow'
+		);
+	});
+
+	// close mobile menu
+	$(".menu-toggle.close").click(function (e) { 
+		e.preventDefault();
+
+		$("#overlay").hide(
+			'slide', {direction: 'right'},
+			'slow'
+		);
+
+		$("#mobile.navigation").hide(
+			'slide', {direction: 'right'}, 
+			'slow'
+		);
+	});
+
+	// after click of nav item, close menu --> how do I do that?
+	// $(".nav-list.mobile .nav-link").click(function (e) { 
+	// 	e.preventDefault();
+	// 	$("#overlay").delay(1000).hide(
+	// 		'slide', {direction: 'right'},
+	// 		'slow'
+	// 	);
+
+	// 	$("#mobile.navigation").delay(1000).hide(
+	// 		'slide', {direction: 'right'}, 
+	// 		'slow'
+	// 	);
+	// });
 });

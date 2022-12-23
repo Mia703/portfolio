f0588d4dc25be7b72a1fd487f243c42945bdcfd3
Mia703 <bookworm7572@gmail.com>
2022-12-22 19:18:31 -0500

    style updates

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
 	const open = $(".menu-toggle.open");
 	const close = $(".menu-toggle.close");
 
+	// open navigation
 	$(open).click(function (e) { 
 		e.preventDefault();
 		$(nav).slideDown("slow");
 	});
 
+	// close navigation
 	$(close).click(function (e) { 
 		e.preventDefault();
 		$(nav).slideUp("slow");
@@ -19,32 +21,4 @@ $(document).ready(function () {
 	var pj_length = $(".project").length;
 	$("#pj-total").html(pj_length);
 
-	// change colour of buttons on hover
-	$(".project-container .button").hover(function (e) {
-			// over
-			$(this).css("background-color", "#584346");
-			$(this).css("border-color", "#584346")
-			$(this).find(".button-link").css("color", "#E8CBCA");
-		}, 
-		function (e) {
-			// out
-			$(this).css("background-color", "#DF7C7D");
-			$(this).css("border-color", "#DF7C7D");
-			$(this).find(".button-link").css("color", "whitesmoke");
-		}
-	);
-
-	$("#up").hover(function (e) {
-		// over
-		$(this).css("background-color", "#584346");
-		$(this).css("border-color", "#584346")
-		$(this).find(".button-link").css("color", "#E8CBCA");
-	}, 
-	function (e) {
-		// out
-		$(this).css("background-color", "#DF7C7D");
-		$(this).css("border-color", "#DF7C7D");
-		$(this).find(".button-link").css("color", "whitesmoke");
-	}
-);
 });

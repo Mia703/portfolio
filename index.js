4086c0c1906efd9b3c87f2ecfe7ec66a218fd87c
Mia703 <bookworm7572@gmail.com>
2023-01-07 19:44:10 -0500

    style redesign

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 $(document).ready(function () {
-
-	const nav = $("nav.navigation");
-	const open = $(".menu-toggle.open");
-	const close = $(".menu-toggle.close");
-
-	// open navigation
-	$(open).click(function (e) { 
-		e.preventDefault();
-		$(nav).slideDown("slow");
-	});
-
-	// close navigation
-	$(close).click(function (e) { 
-		e.preventDefault();
-		$(nav).slideUp("slow");
-	});
-
+	
 	// Count projects
 	var pj_length = $(".project").length;
 	$("#pj-total").html(pj_length);

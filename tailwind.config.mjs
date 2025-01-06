f1e40065ef7471e70937e25bb795d001e853a263
Amya Moore <bookworm7572@gmail.com>
2025-01-06 10:23:55 -0500

    Mobile Nav
    - added linkedin url
    - added github url
    - changed text colour on tablet
    - mobile nav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
         primaryBlack: "#1f1f21",
-        primaryWhite: "#F6F5F5",
+        primaryWhite: "#f9f9f9",
         primaryLightGrey: "#D2D2D2",
-        primaryDarkGrey: "#808080",
+        primaryDarkGrey: "#a7a7a7",
         primaryYellow: "#ffc600",
       },
-		},
-		fontFamily: {
+    },
+    fontFamily: {
       sans: ["Inter", "ui-sans-serif", "system-ui"],
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};

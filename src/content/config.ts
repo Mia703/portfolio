5718aca52dafb9e00302b4e3b2a89ac4809b099e
Amya Moore <bookworm7572@gmail.com>
2025-01-05 20:21:11 -0500

    Changed Photography
    Change cloudinary 'denverZoo' file name to 'photography'

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,10 @@ import { defineCollection } from "astro:content";
 import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
 export const collections = {
-  cloudinary_denverZoo: defineCollection({
+  cloudinary_photography: defineCollection({
     loader: cldAssetsLoader({
       // to display home folder leave empty: ""
-      folder: "denver-zoo",
+      folder: "photography",
       context: true,
     }),
   }),
